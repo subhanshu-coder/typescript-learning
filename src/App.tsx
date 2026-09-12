3f323af2b4374d43a0066fd05deda2c25368ea11
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-12 23:47:13 +0530

    update all

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@
 // import heroImg from './assets/hero.png'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
-import './App.css'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Interface from './component/interface.ts'
+// import './App.css'
+// // import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Interface from './component/interface.ts'
 
-function App() {
-  // const [count, setCount] = useState(0)
+// function App() {
+//   // const [count, setCount] = useState(0)
 
-  return (
-    <div>
-    <Interface />
-      </div>
+//   return (
+//     <div>
+//     <Interface />
+//       </div>
 
-  )
-}
+//   )
+// }
 
-export default App
+// export default App
