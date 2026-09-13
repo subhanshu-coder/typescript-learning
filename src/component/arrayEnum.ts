5bf62d6ce1de6e9075ea8de15a525d504b5da98a
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-13 22:17:01 +0530

    update all

--- src/component/arrayEnum.ts
+++ src/component/arrayEnum.ts
@@ -1,1 +1,9 @@
-const chaiflavours : string [] = ["chai", "jasmine", "ava"];
+const chaiflavours: string [] = ["chai", "jasmine", "ava"];
+const chaiPrice: number [] = [10,20,30];
+
+const rating: Array<number> = [2.5,5.0]
+
+type chaivalue = {
+    name: string;
+    age: number;
+}
