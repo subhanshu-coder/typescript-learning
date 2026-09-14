a006a540f8be5cd3e61e785a06a16e41638879a7
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-14 23:58:50 +0530

    update all

--- src/component/function.ts
+++ src/component/function.ts
@@ -1,7 +1,7 @@
-function makechai(type : string, cups: number) {
-    console.log(`makechai ${cups} cupsof ${type} chai`);
-}
-makechai("masala", 2);
+// function makechai(type : string, cups: number) {
+//     console.log(`makechai ${cups} cupsof ${type} chai`);
+// }
+// makechai("masala", 2);
 
 function createchai (order: {
 name: string,
