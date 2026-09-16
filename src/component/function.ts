f345a0a8072d144dbd984e7526a225c36ed07097
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-16 23:57:30 +0530

    update all

--- src/component/function.ts
+++ src/component/function.ts
@@ -1,29 +1,29 @@
-function makechai(type : string, cups: number) {
-    console.log(`makechai ${cups} cupsof ${type} chai`);
-}
-makechai("masala", 2);
+// function makechai(type : string, cups: number) {
+//     console.log(`makechai ${cups} cupsof ${type} chai`);
+// }
+// makechai("masala", 2);
 
-function createchai (order: {
-name: string,
-price: number
-}): number {
-    return 4
-}
+// function createchai (order: {
+// name: string,
+// price: number
+// }): number {
+//     return 4
+// }
 
-// makechai("ginger", 3);
-function getchaiPrice (): number {
-    return 10;
-}
+// // makechai("ginger", 3);
+// function getchaiPrice (): number {
+//     return 10;
+// }
 
-function makeorder(order:string) {
-    if (!order) return null;
-    return order;
-}
+// function makeorder(order:string) {
+//     if (!order) return null;
+//     return order;
+// }
 
-function logchai(): void{
-    console.log("chai is ready");
-}
+// function logchai(): void{
+//     console.log("chai is ready");
+// }
 
-function orderchai(type :string = "masala", cups: number = 1): string {
-    return `order ${cups} cups of ${type} chai`;
-}
+// function orderchai(type :string = "masala", cups: number = 1): string {
+//     return `order ${cups} cups of ${type} chai`;
+// }
