e911b53c5505a1b956ded4eedc08ab9528946acc
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-17 23:43:53 +0530

    update all

--- src/component/interface.ts
+++ src/component/interface.ts
@@ -11,3 +11,19 @@ const user1: user = {
     name :"prince",
     age: 20
 };
+
+type Teatype = "masala" | "ginger" | "lemon"
+
+function order(t: Teatype) {
+console.log(t);
+}
+
+type basechai = {tealeave : number}
+type extra = {milk : number}
+
+// type masalachai = basechai & extra
+// const cup: masalachai = {
+//     tealeave: 5,
+//     milk: 10
+
+// }
