26ef0c75c1750a4140d0eaa1863ab1d382c4b92e
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-17 23:50:50 +0530

    update all

--- src/component/interface.ts
+++ src/component/interface.ts
@@ -1,7 +1,7 @@
-// interface user {
-//     name : string;
-//     age : number;
-// }
+interface user {
+    name : string;
+    age : number;
+}
 
 function add(a: number, b: number): number {
     return a + b;
