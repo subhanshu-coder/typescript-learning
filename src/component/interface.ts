06a8600c8a86fabd544c575f29d454292a3d0b9b
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-18 20:24:59 +0530

    update all

--- src/component/interface.ts
+++ src/component/interface.ts
@@ -1,29 +1,29 @@
-interface user {
-    name : string;
-    age : number;
-}
+// interface user {
+//     name : string;
+//     age : number;
+// }
 
-function add(a: number, b: number): number {
-    return a + b;
-}
+// function add(a: number, b: number): number {
+//     return a + b;
+// }
 
-const user1: user = {
-    name :"prince",
-    age: 20
-};
+// const user1: user = {
+//     name :"prince",
+//     age: 20
+// };
 
-type Teatype = "masala" | "ginger" | "lemon"
+// type Teatype = "masala" | "ginger" | "lemon"
 
-function order(t: Teatype) {
-console.log(t);
-}
+// function order(t: Teatype) {
+// console.log(t);
+// }
 
-type basechai = {tealeave : number}
-type extra = {milk : number}
+// type basechai = {tealeave : number}
+// type extra = {milk : number}
 
-type masalachai = basechai & extra
-const cup: masalachai = {
-    tealeave: 5,
-    milk: 10
+// type masalachai = basechai & extra
+// const cup: masalachai = {
+//     tealeave: 5,
+//     milk: 10
 
-}
+// }
